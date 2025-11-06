704722d7e7a3346c21e3a35a2b9f62fb02b5ff9b
gregom05 <93887400+gregom05@users.noreply.github.com>
2025-11-06 15:30:05 -0300

    quite las cosas de seidor que copie

--- backend_MG/src/routes/interfaz.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { upsertInterfaz, buscarInterfaz } from '../controllers/interfaz.controller';
-
-const router = Router();
-
-router.post('/', upsertInterfaz);
-router.get('/', buscarInterfaz);
-
-export default router;
